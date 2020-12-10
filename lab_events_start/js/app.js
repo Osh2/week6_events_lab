5df398e8ec9eff55f8f604bcc818584791a2ca35
Osian Edwards-Muthu <osian_rt@hotmail.co.uk>
2020-12-10 16:46:50 +0000

    added event

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
-})
+  const  bookInput = document.querySelector("#new-item-form");
+  bookInput.addEventListener("submit", handleInput);
+
+});
+
+const handleInput = function(event) {
+  event.preventDefault();
+  const readingList = document.querySelector("#reading-list");
+  const title = event.target.title.value;
+  const author = event.target.author.value;
+  const category = event.target.category.value;
+
+  const li = document.createElement("li")
+  li.textContent = `Title: ${title} Author: ${author} Category:${category}` 
+  readingList.append(li)
+}
